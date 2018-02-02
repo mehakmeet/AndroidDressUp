ba6d7479e1e3c3cfceca0b54d8858692224bb5fb
MEHAKMEET <mehakmeet.singh2016@vitstudent.ac.in>
2018-02-03 00:56:56 +0530

    gridview implementation

--- app/src/main/java/startup/mehakmeet/androiddressup/ui/MasterListFragment.java
+++ app/src/main/java/startup/mehakmeet/androiddressup/ui/MasterListFragment.java
@@ -6,22 +6,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 import startup.mehakmeet.androiddressup.R;
+import startup.mehakmeet.androiddressup.data.AndroidImageAssets;
 
 /**
  * Created by MEHAKMEET on 02-02-2018.
  */
 
 public class MasterListFragment extends Fragment {
+
+    GridView mGridView;
+    private ArrayList<Integer> master_img;
+    private Grid_Adapter mGridAdapter;
+
+
     public MasterListFragment() {
     }
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.frag_masterlist,container,false);
 
+
+        mGridView=(GridView)v.findViewById(R.id.masterListGrid);
+
+        master_img=new ArrayList<>();
+        master_img.addAll(AndroidImageAssets.getAll());
+
+        mGridAdapter=new Grid_Adapter(getActivity(),R.layout.grid_items,master_img);
+        mGridView.setAdapter(mGridAdapter);
 
         return v;
     }
